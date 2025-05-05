992cc86a688da5713ca5521812a91fb4c8409f31
Teslim <steslimolayiwola@gmail.com>
2025-05-05 20:36:37 +0100

    home section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import Header from "../Components/Header";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <div className="rounded-lg flex flex-col border w-[1374px] mx-auto mb-10 items-center gap-6">
+      <div className="rounded-lg flex flex-col border mt-[8%] w-[80%] mx-auto mb-10 items-center gap-6">
         <div>
           <h1>
-            Crafting user-centric digital products that delight and inspire - I
-            bring expertise and creativity to every design.
+            Crafting
+            <span className=" text-[#4898A7]">
+              user-centric digital products
+            </span>
+            that
+            <span>delight</span> and <span>inspire </span> - I bring expertise
+            and creativity to every <span>design</span>.
           </h1>
         </div>
         <div>
-          {" "}
           <p>
             Hi, I am Mojeedat, a UX designer with 3+ years of experience
             creating user-centric designs that balance business goals and user
@@ -23,10 +28,9 @@ const Home = () => {
             deliver optimized, user-focused designs.
           </p>
         </div>
-        <div>
-          <p>Feel free to explore other pages or visit our about page.</p>
-          <a href="/about">About</a>
-          <a href="/resume">Resume</a>
+        <div className="flex gap-1">
+          <Link to={"/about"}>About</Link>
+          <Link to={"/resume"}>Resume</Link>
         </div>
       </div>
     </div>
